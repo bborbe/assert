af1b85d06afe4d9f631d506e80516601db9aba2f
Benjamin Borbe <bborbe@rocketnews.de>
2013-08-16 21:40:38 +0200

    add not matcher

---
+++ hamcrest_not.go
@@ -0,0 +1,26 @@
+package assert
+
+type notValueMatcher struct {
+	message        string
+	expectedValue  Matcher
+}
+
+func Not(expectedValue  Matcher) *notValueMatcher {
+	m := new(notValueMatcher)
+	m.expectedValue = expectedValue
+	return m
+}
+
+func (m *notValueMatcher) Message(message string) Matcher {
+	m.message = message
+	return m
+}
+
+func (m *notValueMatcher) Matches(value interface{}) bool {
+	return !m.expectedValue.Matches(value)
+}
+
+func (m *notValueMatcher) DescribeMismatch(value interface{}) error {
+	err := m.expectedValue.DescribeMismatch(value)
+	return buildError("not %s", m.message, err.Error())
+}
