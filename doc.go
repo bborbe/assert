5a041e63de8564b178ad6a3445ddef71fb8e2534
Benjamin Borbe <bborbe@rocketnews.de>
2015-06-11 11:07:53 +0200

    update doc

--- doc.go
+++ doc.go
@@ -1,4 +1,5 @@
-// Copyright 2014 Benjamin Borbe. All rights reserved.
+// Copyright (c) 2015, Benjamin Borbe <bborbe@rocketnews.de>.
+// All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
