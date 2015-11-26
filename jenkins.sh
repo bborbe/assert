8ed8fa331d3b2813d727b7c94b6061c99270d685
Benjamin Borbe <bborbe@rocketnews.de>
2015-11-26 13:13:57 +0100

    use utils

--- jenkins.sh
+++ jenkins.sh
@@ -9,11 +9,11 @@ export REPORT_DIR=${WORKSPACE}/test-reports
 DEB="${NAME}_${VERSION}.deb"
 rm -rf $REPORT_DIR ${WORKSPACE}/*.deb ${WORKSPACE}/pkg
 mkdir -p $REPORT_DIR
-PACKAGES=`cd src && find $SOURCEDIRECTORY -name "*_test.go" | /usr/lib/go/bin/dirof | /usr/lib/go/bin/unique`
+PACKAGES=`cd src && find $SOURCEDIRECTORY -name "*_test.go" | /opt/utils/bin/dirof | /opt/utils/bin/unique`
 FAILED=false
 for PACKAGE in $PACKAGES
 do
-    XML=$REPORT_DIR/`/usr/lib/go/bin/pkg2xmlname $PACKAGE`
+    XML=$REPORT_DIR/`/opt/utils/bin/pkg2xmlname $PACKAGE`
     OUT=$XML.out
     go test -i $PACKAGE
     go test -v $PACKAGE | tee $OUT
