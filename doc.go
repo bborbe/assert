8cd01c845e08bdbb0968eb00cea37551937ceffb
Benjamin Borbe <bborbe@rocketnews.de>
2016-01-04 22:36:21 +0100

    2015 -> 2016

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, Benjamin Borbe <bborbe@rocketnews.de>.
+// Copyright (c) 2016, Benjamin Borbe <bborbe@rocketnews.de>.
 // All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
